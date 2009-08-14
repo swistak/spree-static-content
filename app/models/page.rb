d7a560df31f1acde8729536d0b216b69c3eb5eab
Marcin Raczkowski <marcin.raczkowski@gmail.com>
2009-08-14 13:32:42 +0200

    Adding visible attribute to pages

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,5 +2,6 @@ class Page < ActiveRecord::Base
   validates_presence_of :title
   validates_presence_of :slug
   validates_presence_of :body
-  
+ 
+  named_scope :visible, :conditions => {:visible => true}
 end
