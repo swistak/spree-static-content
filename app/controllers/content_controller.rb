230aa3730ef0418d9e692aa4977aeb85f4e45c2e
Marcin Raczkowski <marcin.raczkowski@gmail.com>
2009-10-16 09:51:28 +0200

    bugfix

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -3,9 +3,9 @@ class ContentController < Spree::BaseController
 
   def show
     path = "page/#{params[:path].join("/")}"
-    pid = Cache.get(path.to_url, 30) do
+    pid = CACHE.fetch(path.to_url, 30) do
       page = Page.find_by_slug(params[:path])
-      page && page
+      page && page.id
     end
     unless pid && (@page = Page.find_by_id(pid))
       render :file => "#{RAILS_ROOT}/public/404.html", :layout => false, :status => 404
